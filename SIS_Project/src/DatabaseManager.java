538373e600997c8ee255af7e1c53dc7ec732a512
littlebugsmith <126014617+littlebugsmith@users.noreply.github.com>
2024-03-21 16:35:55 -0300

    Update DatabaseManager.java
    Added addNewCategory(), addNewManufacturer(), and addProductToStockInv() methods.

--- SIS_Project/src/DatabaseManager.java
+++ SIS_Project/src/DatabaseManager.java
@@ -23,16 +23,59 @@ public class DatabaseManager{
         }
     }
     
+    //Add new category
+    public static void addNewCategory(Category c) throws SQLException {
+        checkConnection(); // Ensure connection is established
+        String query = "insert into category values(?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            // Set parameters
+            statement.setObject(1, c.getName());
+
+            // Execute the query
+            statement.executeUpdate();
+        }
+    }
+
+    //Add a new manufacturer
+    public static void addNewManufacturer(Manufacturer m) throws SQLException {
+        checkConnection(); // Ensure connection is established
+        String query = "insert into manufacturer values(?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            // Set parameters
+            statement.setObject(1, m.getName());
+
+            // Execute the query
+            statement.executeUpdate();
+        }
+    }
+
     // Insert product into the product database
     public static void addNewProduct(Product newProduct) throws SQLException {
         checkConnection(); // Ensure connection is established
-        String query = "insert into products values(?,?,?,?)";
+        String query = "insert into product values(?,?,?,?)";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             // Set parameters
             statement.setObject(1, newProduct.getName());
             statement.setObject(2, newProduct.getCategoryID());
             statement.setObject(3, newProduct.getManufacturerID());
             statement.setObject(4, newProduct.getPrice());
+
+            // Execute the query
+            statement.executeUpdate();
+        }
+    }
+
+    //Add product to the stock inventory
+    public static void addProductToStockInv(InventoryRecord record) throws SQLException {
+        checkConnection(); // Ensure connection is established
+        String query = "insert into stock_inventory values(?,?,?,?,?)";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            // Set parameters
+            statement.setObject(1, record.getProductID());
+            statement.setObject(2, record.getQuantity());
+            statement.setObject(3, record.getLocation());
+            statement.setObject(4, record.getExpiration());
+            statement.setObject(5, record.getDiscount());
 
             // Execute the query
             statement.executeUpdate();
