b42bce10bf6327c797c52a31a57c4c310e9b6fc8
matra <matra@192.168.2.62>
2024-03-21 15:01:30 -0300

    Committing object classes Contributors: Noel, Colin, Walid

---
+++ SIS_Project/src/InventoryRecord.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class InventoryRecord {
+	private int productID;
+	private int quantity;
+	private String location;
+	private LocalDate expirationDate;
+	private BigDecimal discount;
+	
+	public InventoryRecord(int prodID, int qty, String loc, LocalDate expDate, BigDecimal disc) {
+		productID = prodID;
+		quantity = qty;
+		location = loc;
+		expirationDate = expDate;
+		discount = disc;
+	}
+	
+	public int getProductID() {
+		return productID;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public LocalDate getExpiration() {
+		return expirationDate;
+	}
+	
+	public BigDecimal getDiscount() {
+		return discount;
+	}
+}
